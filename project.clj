531b8e278bd4a3ef3627389380d8fb236065f4f3
Fabio Yamate <fabioyamate@gmail.com>
2014-09-26 23:11:29 -0300

    Add project dependencies

--- project.clj
+++ project.clj
@@ -3,4 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-alpha2"]])
+  :dependencies [[org.clojure/clojure "1.7.0-alpha2"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [http-kit "2.1.16"]
+                 [cheshire "5.3.1"]
+                 [compojure "1.1.8"]
+                 [ring/ring-defaults "0.1.1"]])

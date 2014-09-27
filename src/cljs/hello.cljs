dc00058cf7729861997bda65d1ef5ec5fdeec080
Ricardo de Cillo <ricardo.cillo@plataformatec.com.br>
2014-09-27 20:22:49 -0300

    Example using channels for consuming websocket events

--- src/cljs/hello.cljs
+++ src/cljs/hello.cljs
@@ -1,15 +1,23 @@
 (ns example.hello
-  (:require [cognitect.transit :as t]))
+  (:require [cognitect.transit :as t]
+            [cljs.core.async :refer [<! >! chan]])
+  (:require-macros [cljs.core.async.macros :refer [go]]))
+
 
 (defn log [text]
   (.log js/console text))
 
 (def r (t/reader :json))
 
+(def tweets-channel (chan))
+
 (def socket (js/WebSocket. "ws://localhost:8080/ws"))
 
-(set! socket.onmessage (fn [e] (log (:msg (t/read r e.data)))))
-(set! socket.onerror (fn [e] (log (:msg (t/read r e.data)))))
+(set! socket.onmessage (fn [e] (go (>! tweets-channel (:msg (t/read r e.data))))))
+(set! socket.onerror (fn [e] (go (>! tweets-channel (:msg (t/read r e.data))))))
 
 (.setTimeout js/window (fn []
                          (.close socket)) 3000)
+
+(go (while true
+      (log (<! tweets-channel))))
